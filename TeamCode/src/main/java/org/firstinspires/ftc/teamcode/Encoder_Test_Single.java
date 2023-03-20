ed7fd7b3d92a568043f4213d82353c6d2ea383d7
EngiNERDS <Enginerds@tccs.org>
2023-03-20 09:07:39 -0700

    NPC_Code

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Encoder_Test_Single.java
@@ -0,0 +1,97 @@
+package org.firstinspires.ftc.teamcode;
+
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@Autonomous(name="Encoder Test_Single",group="used")
+//@Disabled
+public class Encoder_Test_Single extends LinearOpMode {
+
+//Declare Motors and Deivces needed to run the code.
+
+    //Motors
+    // The private/public DcMotor _____; are used to identify a motor that can be used throughout the code.
+    // Note that it does not matter if you use Public or a Private class identity for the motor
+    // EX: Doesn't matter if you do Private DcMotor or Public DcMotor. They are interchangeable
+    // should look like ______ DcMotor "______"
+    //                  Class            Name
+    private DcMotor motorFL;
+   // private DcMotor motorFR;
+   // private DcMotor motorBL;
+    //private DcMotor motorBR;
+
+
+
+    @Override
+    public void runOpMode() {
+
+        // HardwareMap Section (Used to talk to the driver hub for the configuration)
+
+        // Motors
+        motorFL = hardwareMap.dcMotor.get("motorFL");
+      //  motorFR = hardwareMap.dcMotor.get("motorFR");
+       // motorBL = hardwareMap.dcMotor.get("motorBL");
+        //motorBR = hardwareMap.dcMotor.get("motorBR");
+
+
+        //Encoders
+        // Has the Run_Using_Encoders because all the motors use Encoders throughout the code.
+        // You have to have this if you are running encoders.
+        motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //motorFR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+       // motorBL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);//motorBR.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+
+        waitForStart();
+            // The stop and reset encoders is needed to reset and start the encoders (You need one at the
+            // end because it tells the robot where the drive code starts and ends, kinda like brackets)
+            motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+           // motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+           // motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+           // motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            // This sets the direction for the motor for the wheels to drive forward
+            motorFL.setDirection(DcMotorSimple.Direction.FORWARD);
+            //motorBL.setDirection(DcMotorSimple.Direction.FORWARD);
+           // motorFR.setDirection(DcMotorSimple.Direction.REVERSE);
+           // motorBR.setDirection(DcMotorSimple.Direction.REVERSE);
+            // Gives it a position to run to
+            //motorFL.setTargetPosition(36 * 45);
+           // motorFR.setTargetPosition(36 * 45);
+           // motorBL.setTargetPosition(36 * 45);
+           // motorBR.setTargetPosition(36 * 45);
+            // tells it to go to the position that is set
+            //motorFL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            //motorFL.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        //motorFR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            //motorBL.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+           // motorBR.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            // the motor speed for Wheels
+
+            telemetry.addData("1","motorFL =" + String.format("%d", motorFL.getCurrentPosition()));
+            telemetry.update();
+            motorFL.setPower(.5);
+            sleep(2000);
+            motorFL.setPower(0);
+            // motorFR.setPower(.5);
+            //motorBL.setPower(.5);
+           // motorBR.setPower(.5);
+            // While loop keeps the code running until motors reach the desired position
+            //while (opModeIsActive() && ( motorFL.isBusy() ) ) {
+            //}
+           // motorFL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+           // motorFR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            //motorBL.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+            //motorBR.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+
+        telemetry.addData("1","motorFL =" + String.format("%d", motorFL.getCurrentPosition()));
+        telemetry.update();
+        sleep(2000);
+    }
+
+
+}//Closes Linear Op Mode
